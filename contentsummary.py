5f9b2eb6296b85663ed502347e86834d4a3140b8
Esha Chakraborty <ec3074@columbia.edu>
2015-11-03 09:49:09 -0500

    update

--- contentsummary.py
+++ contentsummary.py
@@ -5,24 +5,27 @@ import urllib
 import json
 
 def getTop4url(query):
-	url = "http://api.bing.net/json.aspx?AppId=" + "7Mtcdr7rWaH+3i2WOB4KaKZbAVdwptuX6dG/m7EBCz8" + "&Version=2.2&Market=en-US&Query=" + query + "&Sources=web&Web.Count=20&JsonType=raw"
-	temp = urllib.urlopen(url)
-	
+	bingurl = "http://api.bing.net/json.aspx?AppId=" + "7Mtcdr7rWaH+3i2WOB4KaKZbAVdwptuX6dG/m7EBCz8" + "&Version=2.2&Market=en-US&Query=" + query + "&Sources=web&Web.Count=20&JsonType=raw"
+	response = urllib.urlopen(url)
+	content = response.read()
+
 	url4 = []
+
 	return url4
 
 
 def summarize(listdir, url):
 	for directory in listdir:
 		try:
-			f1 = open(directory + '.txt','r')
+			#this approach will also take 
+			f1 = open(directory + '.txt')
 		except Exception:
 			sys.exit('No such directory')
 
 	countdict = defaultdict(int)
 
 	for lines in f1:
-		terms = line.strip().split(" ")
+		terms = lines.strip().split(" ")
 		keywords = '+'.join(terms[1:])
 
 		url4  = getTop4url(url + keywords)
@@ -31,11 +34,16 @@ def summarize(listdir, url):
 			newproc = subprocess.Popen(['lynx','-dump',url], stdout=subprocess.PIPE)
 			data, err 	= newproc.communicate()
 			ref = data.find('\nReferences\n')
-			before_ref = data[:refPoint].lower()
-
+			before_ref = data[:ref].lower()
 			wordlist = re.findall('[a-z]+',before_ref)
 
-			for word in wordList:
+			for word in wordlist:
 				countdict[word] += 1
+	f2 = open('sample-' + directory + '.txt','w')
+	for word,count in sorted(countdict.items()):
+		f2.write(word + '  ' + str(count) + '\n')
+
+	f1.close()
+	f2.close()
 
 
