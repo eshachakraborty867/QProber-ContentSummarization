46b14aadae7dc8e9bcd6bf7a6b7ef0f53c6d71eb
Esha Chakraborty <ec3074@columbia.edu>
2015-11-03 09:59:04 -0500

    update

--- contentsummary.py
+++ contentsummary.py
@@ -17,38 +17,37 @@ def getTop4url(query):
 def summarize(listdir, url):
 	for directory in listdir:
 		try:
-			#this approach will also take 
 			f1 = open(directory + '.txt')
+			countdict = defaultdict(int)
+			countdoc  = defaultdict(int)
+
+			for lines in f1:
+				terms = lines.strip().split(" ")
+				keywords = '+'.join(terms[1:])
+
+				url4  = getTop4url(url + keywords)
+
+				for eachurl in url4:
+					newproc = subprocess.Popen(['lynx','-dump',url], stdout=subprocess.PIPE)
+					data, err 	= newproc.communicate()
+					ref = data.find('\nReferences\n')
+					before_ref = data[:ref].lower()
+					wordlist = re.findall('[a-z]+',before_ref)
+
+					if word in wordlist:
+						countdoc[word] += 1
+
+					for word in wordlist:
+						countdict[word] += 1
+
+			f2 = open('sample-' + directory + '.txt','w')
+			for word,count in sorted(countdict.items()):
+				f2.write(word + '  ' + str(count) + '  ' + str(countdoc[word])+ '\n')
+
+			f1.close()
+			f2.close()
+
 		except Exception:
 			sys.exit('No such directory')
-
-	countdict = defaultdict(int)
-	countdoc  = defaultdict(int)
-
-	for lines in f1:
-		terms = lines.strip().split(" ")
-		keywords = '+'.join(terms[1:])
-
-		url4  = getTop4url(url + keywords)
-
-		for eachurl in url4:
-			newproc = subprocess.Popen(['lynx','-dump',url], stdout=subprocess.PIPE)
-			data, err 	= newproc.communicate()
-			ref = data.find('\nReferences\n')
-			before_ref = data[:ref].lower()
-			wordlist = re.findall('[a-z]+',before_ref)
-
-			if word in wordlist:
-				countdoc[word] += 1
-
-			for word in wordlist:
-				countdict[word] += 1
-
-	f2 = open('sample-' + directory + '.txt','w')
-	for word,count in sorted(countdict.items()):
-		f2.write(word + '  ' + str(count) + '  ' + str(countdoc[word])+ '\n')
-
-	f1.close()
-	f2.close()
 
 
